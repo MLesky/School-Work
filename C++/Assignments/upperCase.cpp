5083e6084b0bbbbe7fdd57343df555011af4addb
MbLespa <mbahlesky2@gmail.com>
2023-01-09 17:45:43 +0100

    upper and sentence case

--- C++/Assignments/upperCase.cpp
+++ C++/Assignments/upperCase.cpp
@@ -2,14 +2,24 @@
 
 using namespace std;
 
-string sentenceCase(string text){
+string upperCase(string text){
+
+    // capitallise the whole text
     int i;
+    for(i = 0; i < text.length(); i++){
+        text[i] = toupper(text[i]);
+    }
+
+    return text;
+}
+
+string sentenceCase(string text){
 
     // capitallise the first letter
     text[0] = toupper(text[0]);
 
-
     // capitallise the first letter of each word
+    int i;
     for(i = 0; i < text.length() - 1; i++){
         if(text[i] == ' '){
             text[i+1] = toupper(text[i+1]);
@@ -19,7 +29,12 @@ string sentenceCase(string text){
     return text;
 }
 
+
 int main(){
-    
+    string sentence = "my name is mbah lesky";
+    string name = "mbah lesky";
+
+    cout << "\"" << name << "\"" << " in upper case is " << "\"" << upperCase(name) << "\"\n";
+    cout << "\"" << sentence << "\"" << " in sentence case is " << "\"" << sentenceCase(sentence) << "\"\n";
     return 0;
 }
